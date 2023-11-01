1f3c8711a8d1acb33585fb7a9ea81daeb59722ae
Anirup Patnaik <anirup049@gmail.com>
2023-11-01 21:57:41 +0530

    Certification Component

---
+++ src/components/certificates/Certificates.jsx
@@ -0,0 +1,90 @@
+import {
+  Card,
+  CardContent,
+  Typography,
+  Box,
+  Divider,
+  Grid,
+  Paper,
+} from "@mui/material";
+import React from "react";
+import { FaDownload } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import styles from "./Certificates.module.scss";
+
+const Certificate = ({ title, issuer, date }) => (
+  <div className={styles.certificate}>
+    <Card sx={{ borderRadius: 5 }}>
+      <CardContent
+        className={styles.content}
+        sx={{
+          padding: 0,
+          "&:last-child": { pb: 0 },
+          minHeight: 60,
+        }}
+      >
+        <Grid container XS={12} spacing={0} sx={{ minHeight: 60 }}>
+          <Grid
+            item
+            xs={2}
+            className={styles.grid}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              backgroundColor: "black",
+              color: "white",
+            }}
+          >
+            <div className={styles.month}>Oct</div>
+            <div className={styles.year}>2023</div>
+          </Grid>
+          <Grid item xs={8} className={styles.grid}>
+            <div className={styles.title}>
+              Exam AZ-900: Microsoft Azure Fundamentals.
+            </div>
+          </Grid>
+          <Grid
+            item
+            xs={1}
+            className={[styles.grid, styles.actions]}
+            sx={{
+              backgroundColor: "black",
+              color: "white",
+            }}
+          >
+            <FaDownload size={25} />
+          </Grid>
+          <Grid
+            item
+            xs={1}
+            className={[styles.grid, styles.actions]}
+            sx={{
+              backgroundColor: "black",
+              color: "white",
+            }}
+          >
+            <AiFillDelete size={25} />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  </div>
+);
+
+const Certificates = ({ certificates }) => (
+  <div>
+    <Typography sx={{ textAlign: "left", fontSize: 20 }}>
+      My Certifications
+    </Typography>
+    {certificates.map((certificate, index) => (
+      <Certificate
+        key={index}
+        title={certificate.title}
+        issuer={certificate.issuer}
+        date={certificate.date}
+      />
+    ))}
+  </div>
+);
+
+export default Certificates;
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,18 +1,35 @@
 import Box from "@mui/material/Box";
 import NavBar from "../../components/navBar/NavBar";
 import SideNav from "../../components/sideNav/SideNav";
-// import styles from "./Home.module.scss";
+import Certificates from "../../components/certificates/Certificates";
+import styles from "./Profile.module.scss";
+import { Typography } from "@mui/material";
 
 const Profile = () => {
+  const certificatesData = [
+    {
+      title: "Certificate 1",
+      issuer: "Issuer 1",
+      date: "May 2022",
+    },
+    {
+      title: "Certificate 2",
+      issuer: "Issuer 2",
+      date: "June 2023",
+    },
+    // Add more certificate objects as needed
+  ];
   return (
     <>
       <NavBar />
       <Box height={70} />
       <Box sx={{ display: "flex" }}>
-        {/* <Toolbar /> */}
         <SideNav />
-        <h1>Profile</h1>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}></Box>
+        <Box sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <Certificates certificates={certificatesData} />
+          </Box>
+        </Box>
       </Box>
     </>
   );
